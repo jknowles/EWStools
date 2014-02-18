1f8c3cb6321de800cdbca7c6e181f6f7240a92b7
Jared Knowles <jknowles@gmail.com>
2014-02-18 14:59:57 -0600

    modsearch tests in place

--- inst/run-all.R
+++ inst/run-all.R
@@ -3,6 +3,8 @@ source("R/balanceFactors.R")
 source("R/confusion_matrix.R")
 source("R/robustPE.R")
 source("R/ROCtest.R")
-
+source("R/modsearch.R")
 
 test_file("inst/tests/test-caretobjects.R")
+test_file("inst/tests/test-glmobjects.R")
+test_file("inst/tests/test-modsearch.R")
--- inst/tests/test-modsearch.R
+++ inst/tests/test-modsearch.R
@@ -5,7 +5,7 @@
 # others?!?
 
 
-context("Test train objects")
+context("Test summaries of train objects")
 
 set.seed(442)
 library(caret)
@@ -29,9 +29,82 @@ fullModel <- train(Class ~ ., data = train,
 modobj <- modAcc(fullModel, datatype = c("train", "test"), modelKeep = FALSE, 
                  testdata = list(preds = test[, -19], class = test[, 19]))
 
-dfExtract(modobj)
-
-modobj <- modAcc(fullModel, datatype = c("test"), modelKeep = FALSE, 
+modobj2 <- modAcc(fullModel, datatype = c("test"), modelKeep = FALSE, 
                  testdata = list(preds = test[, -19], class = test[, 19]))
 
-dfExtract(modobj)
+modobj3 <- modAcc(fullModel, datatype = c("train"), modelKeep = FALSE)
+
+
+test_that("modAcc produces named lists", {
+  expect_that(modobj, is_a("list"))
+  expect_that(modobj2, is_a("ROCit"))
+  expect_that(modobj3, is_a("ROCit"))
+})
+
+test_that("modAcc throws messages", {
+  expect_that(modAcc(fullModel, datatype = c("train"), modelKeep = FALSE), 
+              shows_message())
+})
+
+test_that("modAcc list has correct slots", {
+  expect_that(modobj$method, is_a("character"))
+  expect_that(modobj$time, is_a("numeric"))
+  expect_that(modobj$summaryTr, is_a("ROCit"))
+  expect_that(modobj$summaryTe, is_a("ROCit"))
+})
+
+test_that("modAcc reports NULL for slots without objects", {
+  expect_identical(modobj2$summaryTr, "NULL")
+  expect_identical(modobj3$summaryTe, "NULL")
+  
+})
+
+test_that("modAcc subobjects have correct slots for train data", {
+  expect_that(modobj$summaryTr@thresh, is_a("numeric"))
+  expect_that(modobj$summaryTr@auc, is_a("numeric"))
+  expect_that(modobj$summaryTr@confusematrix, is_a("data.frame"))
+  expect_that(modobj$summaryTr@rarepercent, is_a("numeric"))
+  expect_that(modobj$summaryTr@falsepositive, is_a("numeric"))
+  expect_that(modobj$summaryTr@rocobj, is_a("roc"))
+  expect_that(modobj$summaryTr@modtype, is_a("character"))
+  expect_that(modobj$summaryTr@modcall, is_a("character"))
+  expect_that(modobj$summaryTr@datatype, is_a("character"))
+  expect_identical(modobj$summaryTr@datatype, "train")  
+  expect_that(modobj$summaryTe@thresh, is_a("numeric"))
+  expect_that(modobj$summaryTe@auc, is_a("numeric"))
+  expect_that(modobj$summaryTe@confusematrix, is_a("data.frame"))
+  expect_that(modobj$summaryTe@rarepercent, is_a("numeric"))
+  expect_that(modobj$summaryTe@falsepositive, is_a("numeric"))
+  expect_that(modobj$summaryTe@rocobj, is_a("roc"))
+  expect_that(modobj$summaryTe@modtype, is_a("character"))
+  expect_that(modobj$summaryTe@modcall, is_a("character"))
+  expect_that(modobj$summaryTe@datatype, is_a("character"))
+  expect_identical(modobj$summaryTe@datatype, "test")  
+})
+
+# test_that("ROCit objects pass best threshold parameters through", {
+#   expect_more_than(res3@thresh, res2@thresh)
+#   expect_more_than(res3@rarepercent, res2@rarepercent)
+#   expect_more_than(res3@falsepositive, res2@falsepositive)
+#   expect_equal(res2@auc, res3@auc)
+# })
+
+context("Converting modAcc lists to dataframes for plotting")
+
+test_that("A data.frame can be extracted from model accuracy objects...", {
+  expect_that(dfExtract(modobj), is_a("data.frame"))
+  expect_that(dfExtract(modobj2), is_a("data.frame"))
+  expect_that(dfExtract(modobj3), is_a("data.frame"))
+})
+
+test_that("dfExtract functions when only test or training data present", {
+  expect_that(nrow(dfExtract(modobj)), is_more_than(nrow(dfExtract(modobj2))))
+  expect_that(nrow(dfExtract(modobj)), is_more_than(nrow(dfExtract(modobj3))))
+  expect_identical(nrow(dfExtract(modobj2)), nrow(dfExtract(modobj3)))
+})
+
+# 
+# class(dfExtract(modobj))
+# 
+# 
+# dfExtract(modobj)
