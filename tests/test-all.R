2bc3cf3dd8ca0a578c1bee746d9725b27f2b895a
Jared Knowles <jknowles@gmail.com>
2014-02-23 17:45:12 -0600

    fixup tests directory for testthat update

---
+++ tests/test-all.R
@@ -0,0 +1,14 @@
+library(testthat)
+test_check("EWS_tools")
+
+
+# source("R/balanceFactors.R")
+# source("R/confusion_matrix.R")
+# source("R/robustPE.R")
+# source("R/ROCtest.R")
+# source("R/modsearch.R")
+# 
+# test_file("inst/tests/test-caretobjects.R")
+# test_file("inst/tests/test-glmobjects.R")
+# test_file("inst/tests/test-modsearch.R")
+#test_file("test-utilities")
