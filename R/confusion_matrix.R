17e2a7a8770e2220a59640ebe7232f62cb4ddb76
Jared Knowles <jknowles@gmail.com>
2014-03-03 10:18:37 -0600

    prep for roxygen

--- R/confusion_matrix.R
+++ R/confusion_matrix.R
@@ -1,3 +1,6 @@
+
+##' @title An internal function for creating the confusion matrix for \code{\linkS4class{ROCit}} 
+##' objects
 confuse_mat <- function(mod, thresh, prop = FALSE, testdata){
   if(!missing(testdata)){
     dv <- as.character(mod$formula[[2]])
@@ -36,76 +39,9 @@ confuse_mat <- function(mod, thresh, prop = FALSE, testdata){
 }
 
 
-###################################
-# For VGLM objects (S4)
-###################################
-
-################################################################################
-# Title: A function to create a confusion matrix out of a model object
-# x = an S4 object, VGLM
-# thresh = a value from 0 to 1 to assign binary responses to
-# prop = a logical vector indicating if proportions instead of counts shoud be
-#         returned
-# Returns a data.frame
-################################################################################
-
-
-confuse_mat.vglm <- function(x, thresh, prop,...){
-  tp  <- length(x@y[x@y > 0 & predict(x, type="response") >= thresh])
-  fp  <- length(x@y[x@y < 1 & predict(x, type="response") >= thresh])
-  fn  <- length(x@y[x@y > 0 & predict(x, type="response") <= thresh])
-  tn  <- length(x@y[x@y < 1 & predict(x, type="response") <= thresh])
-  mat <- data.frame("Predicted NC" = c(tn, fn), "Predicted C" = c(fp, tp))
-  row.names(mat) <- c("Actual NC", "Actual C")
-  ifelse(prop==TRUE, return((mat/length(x@y))*100), return(mat))
-}
-
-################################
-# For TRAIN objects
-################################
-
-################################################################################
-# Title: A function to create a confusion matrix out of a model object
-# x = a train model fit object (from caret)
-# preds = a data.frame of the predictors
-# thresh = a value from 0 to 1 to assign binary responses to
-# prop = a logical vector indicating if proportions instead of counts shoud be
-#         returned
-# 
-# Returns a data.frame
-################################################################################
-
-confuse_mat.train <- function(mod, thresh, prop = FALSE, newdata = NULL){
-  if(!missing(newdata)){
-    mypred <- predict(mod, newdata=newdata$preds, type="prob")[1]
-    y <- as.character(newdata$class)
-  } else {
-    mypred <- predict(mod, type="prob")
-    y <- as.character(mod$trainingData$.outcome)
-  }
-  
-  statmod <- function(x) {
-    z <- table(as.vector(x))
-    names(z)[z == max(z)]
-  }
-  
-  mode <- statmod(y)
-  y[y == mode] <- "1"
-  y[y != "1"] <- 0
-  y <- as.character(y)
-  y <- as.numeric(y)
-  # normalize preds
-  mypred <- mypred[, 1]
-  tp <- length(y[y > 0 & mypred >= thresh])
-  fp <- length(y[y < 1 & mypred >= thresh])
-  fn <- length(y[y > 0 & mypred <= thresh])
-  tn <- length(y[y < 1 & mypred <= thresh])
-  mat <- data.frame("Predicted NC"=c(tn, fn), "Predicted C"=c(fp, tp))
-  row.names(mat) <- c("Actual NC", "Actual C")
-  ifelse(prop==TRUE, return((mat / length(y)) * 100), return(mat))
-}
-
-confuse_mat.train2 <- function(x, thresh){
+##' @title An internal function for creating the confusion matrix for \code{\linkS4class{ROCit}} 
+##' objects from train
+confuse_mat.train <- function(x, thresh){
   x$yn <- as.character(x$obs)
   statmod <- function(x) {
     z <- table(as.vector(x))
--- tests/test-all.R
+++ tests/test-all.R
@@ -3,12 +3,12 @@ library(EWStools)
 test_check("EWStools")
 
 
-# source("R/balanceFactors.R")
-# source("R/confusion_matrix.R")
-# source("R/ROCtest.R")
-# source("R/modsearch.R")
-# 
-# test_file("inst/tests/test-caretobjects.R")
-# test_file("inst/tests/test-glmobjects.R")
-# test_file("inst/tests/test-modsearch.R")
-#test_file("test-utilities")
+source("R/balanceFactors.R")
+source("R/confusion_matrix.R")
+source("R/ROCtest.R")
+source("R/modsearch.R")
+
+test_file("tests/testthat/test-caretobjects.R")
+test_file("tests/testthat/test-glmobjects.R")
+test_file("tests/testthat/test-modsearch.R")
+# test_file("tests/testthat/test-utilities.R")
