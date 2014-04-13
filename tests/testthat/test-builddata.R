8a23cc04c11f3936c29d7ca373f6eba0183cb120
Jared Knowles <jknowles@gmail.com>
2014-04-13 12:45:43 -0500

    auto-omit linear combos in place now

--- tests/testthat/test-builddata.R
+++ tests/testthat/test-builddata.R
@@ -1,40 +1,56 @@
-
-context("Test building of data")
-
-set.seed(442)
-library(caret)
-full <- twoClassSim(n = 1500, intercept = -8, linearVars = 1, 
-                     noiseVars = 1, corrVars = 1, corrValue = 0.6)
-# test <- twoClassSim(n = 1000, intercept = -7, linearVars = 1, 
-#                     noiseVars = 1, corrVars = 1, corrValue = 0.6)
-
-idx <- createDataPartition(full[, "Class"], 1, p = .25)
-train <- full[idx[[1]], ]
-test <- full[-idx[[1]], ]
-
-
-split1 <- splitData(full, class = "Class", p = 0.25)
-split2 <- splitData(full, class = "Class", p = 0.5)
-
-
-full$Factor1 <- sample(letters[1:5], nrow(full), replace=TRUE)
-full$Factor2 <- sample(letters[9:14], nrow(full), replace=TRUE)
-
-prednames <- c("TwoFactor1", "TwoFactor2", "Linear1", "Nonlinear1", "Nonlinear3", 
-              "Corr1", "Factor1", "Factor2")
-
-
-
-head(buildModelMatrix(data = full, predvars = prednames, na.omit=TRUE))
-
-
-
-
-table(split1$test$Class)
-table(split1$train$Class)
-
-zed <- assembleData(full, class = "Class", p = 0.25, predvars = prednames)
-
-zed <- assembleData(full[full$Factor1 == "e" | 
-                           full$Factor1 == "d",], 
-                    class = "Class", p = 0.25, predvars = prednames)
+# 
+# context("Test building of data")
+# 
+# set.seed(442)
+# library(caret)
+# full <- twoClassSim(n = 1500, intercept = -8, linearVars = 1, 
+#                      noiseVars = 1, corrVars = 1, corrValue = 0.6)
+# # test <- twoClassSim(n = 1000, intercept = -7, linearVars = 1, 
+# #                     noiseVars = 1, corrVars = 1, corrValue = 0.6)
+# 
+# idx <- createDataPartition(full[, "Class"], 1, p = .25)
+# train <- full[idx[[1]], ]
+# test <- full[-idx[[1]], ]
+# 
+# 
+# split1 <- splitData(full, class = "Class", p = 0.25)
+# split2 <- splitData(full, class = "Class", p = 0.5)
+# 
+# 
+# full$Factor1 <- sample(letters[1:5], nrow(full), replace=TRUE)
+# full$Factor2 <- sample(letters[9:14], nrow(full), replace=TRUE)
+# 
+# prednames <- c("TwoFactor1", "TwoFactor2", "Linear1", "Nonlinear1", "Nonlinear3", 
+#               "Corr1", "Factor1", "Factor2")
+# 
+# 
+# 
+# head(buildModelMatrix(data = full, predvars = prednames, na.omit=TRUE))
+# 
+# 
+# 
+# 
+# table(split1$test$Class)
+# table(split1$train$Class)
+# 
+# zed <- assembleData(full, class = "Class", p = 0.25, predvars = prednames)
+# 
+# zed <- assembleData(full[full$Factor1 == "e" | 
+#                            full$Factor1 == "d",], 
+#                     class = "Class", p = 0.25, predvars = prednames)
+# 
+# context("Test linear combos omission")
+# 
+# testData1 <- matrix(0, nrow=20, ncol=8)
+# testData1[,1] <- 1
+# testData1[,2] <- round(rnorm(20), 1)
+# testData1[,3] <- round(rnorm(20), 1)
+# testData1[,4] <- round(rnorm(20), 1)
+# testData1[,5] <- 0.5 * testData1[,2] - 0.25 * testData1[,3] - 0.25 * testData1[,4]
+# testData1[1:4,6] <- 1
+# testData1[5:10,7] <- 1
+# testData1[11:20,8] <- 1
+# 
+# findLinearCombos(testData1)
+# 
+# omitLinearCombos(testData1)
